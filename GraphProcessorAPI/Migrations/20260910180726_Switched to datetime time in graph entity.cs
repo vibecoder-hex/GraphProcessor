e46201d884a33becbd892118f700d7a673128245
vibecoderhex <rty.sem@yandex.ru>
2026-09-26 12:02:24 +0300

    added new migrations

---
+++ GraphProcessorAPI/Migrations/20260910180726_Switched to datetime time in graph entity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraphProcessorAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Switchedtodatetimetimeingraphentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "creationat",
+                table: "graph",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "creationat",
+                table: "graph",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
---
+++ GraphProcessorAPI/Migrations/20260918154701_Added Image column in Graph entity.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraphProcessorAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedImagecolumninGraphentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "image",
+                table: "graph",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "image",
+                table: "graph");
+        }
+    }
+}
